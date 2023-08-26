ce7ac327f72d97dac87223d391c15cf9b1b55eb4
taufanp <taufanprihantoro15@gmail.com>
2023-08-26 23:36:57 +0700

    ⭐ Unit test for Book List View Model

--- src/core/presentation/useBookListViewModel/useBookListViewModel.test.ts
+++ src/core/presentation/useBookListViewModel/useBookListViewModel.test.ts
@@ -5,21 +5,26 @@ import {BookRepository} from '../../domain/repositories/book';
 import {when} from 'jest-when';
 import {wrapper} from '../../../config/reactQuery';
 
-const repo: BookRepository = mock();
+let repository: BookRepository;
+
+beforeEach(() => {
+  repository = mock();
+  when(repository.getBooks).mockResolvedValue([
+    {id: 'test1', title: 'test1', description: '', price: 0, cover: ''},
+    {id: 'test2', title: 'tes2', description: '', price: 0, cover: ''},
+  ]);
+});
 
 describe('Book List View Model', () => {
   it('Should return list of books', async () => {
-    const {result} = renderHook(() => useBookListViewModel(repo), {wrapper});
-
-    when(repo.getBooks).mockResolvedValue([
-      {id: 'test', title: 'test', description: '', price: 0, cover: ''},
-      {id: 'test', title: 'test', description: '', price: 0, cover: ''},
-    ]);
+    const {result} = renderHook(() => useBookListViewModel(repository), {
+      wrapper,
+    });
 
     await waitFor(() => {
       expect(result.current.books).toHaveLength(2);
-      expect(result.current?.books?.[0]?.title).toEqual('test');
-      expect(repo.getBooks).toHaveBeenCalledTimes(1);
+      expect(result.current?.books?.[0]?.title).toEqual('test1');
+      expect(repository.getBooks).toHaveBeenCalledTimes(1);
     });
   });
 });
